7e98a4e1a38f11d5a54b7f945eedd06799e649f2
ekatsevi <katsevich.gene@gmail.com>
2019-12-23 14:25:19 -0500

    Added machine as parameter to submission script to set paths properly

--- run_one_precomputation.R
+++ run_one_precomputation.R
@@ -1,13 +1,17 @@
 # rm(list = ls())
 args <- commandArgs(trailingOnly = TRUE)
 
+print(args)
+print(args[1])
+print(args[2])
+print(args[3])
+print(args[4])
+
 # parse arguments
 experiment_name = args[1]
 b = as.integer(args[2])
 base_dir = args[3]
 machine = args[4]
-
-print(args)
 
 print(machine)
 
