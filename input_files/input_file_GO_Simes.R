059b727ddfe4eefd3ec9cde1153e7b29157466ce
ekatsevi <katsevich.gene@gmail.com>
2019-12-24 12:21:46 -0500

    improved code organization

--- input_files/input_file_GO_Simes.R
+++ input_files/input_file_GO_Simes.R
@@ -31,6 +31,7 @@ if(num_args == 0){
 }
 if(num_args == 1){
   mode = args[1]
+  print(mode)
   stopifnot("mode" %in% c("num_experiments", "num_precomputations"))
   if(mode == "num_experiments"){
     cat(nrow(parameters))
