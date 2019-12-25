c10ca82cca45f8eb3a18b101a13808372bf72e69
ekatsevi <katsevich.gene@gmail.com>
2019-12-24 21:19:37 -0500

    Packaged Focused BH functions together and prepared for first numerical experiment

--- run_one_experiment.R
+++ run_one_experiment.R
@@ -1,11 +1,13 @@
-rm(list = ls())
-source("setup.R")
+# rm(list = ls())
 args <- commandArgs(trailingOnly = TRUE)
 
 # parse arguments
 experiment_name = args[1]
 experiment_index = as.integer(args[2])
 base_dir = args[3]
+machine = args[4]
+
+source("setup.R", local = TRUE)
 
 # run the computation
 run_one_experiment(experiment_name, experiment_index, base_dir)
